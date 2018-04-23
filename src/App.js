b45185f80fcad6fd8a69e881243655ef66ef649a
Andrew McCafferty <andrew.mccafferty@theappbusiness.com>
2018-04-23 21:52:51 +0100

    refactor game flow

--- src/App.js
+++ src/App.js
@@ -17,8 +17,11 @@ class App extends Component {
         showSpecies: false,
         speciesList: [],
         selectedSpeciesGuess: null,
-        guessCorrect: false,
-        counter: 0
+        counter: 0,
+        correctCount: 0,
+        errorLoading: false,
+        expertMode: false,
+        guessCorrect: false
         };
     this.callApi('species').then((result) => {
         this.setState((prevState, props) => {
@@ -39,15 +42,34 @@ class App extends Component {
         return body;
     };
 
+  clearGuess = () => {
+      if (this._typeahead) {
+          this._typeahead.getInstance().clear();
+      }
+      if (this.state.selectedSpeciesGuess) {
+          this.setState((prevState, props) => {
+             return {
+                 ...props,
+                 selectedSpeciesGuess: null
+             }
+          });
+      }
+  }
   getRandomBirdsong = function() {
-
+      this.clearGuess();
       this.setState((prevState, props) => {
           return {
               ...props,
-              loading: true
+              errorLoading: false,
+              loading: true,
+              guessCorrect: false
           }
-      })
-      this.callApi('birdsong').then(result => {
+      });
+      var randomRecordingApiUrl = 'birdsong';
+      if (!this.state.expertMode) {
+          randomRecordingApiUrl += "?level=1";
+      }
+      this.callApi(randomRecordingApiUrl).then(result => {
           this.setState((prevState, props) => {
               if (result.noRecordings) {
                   return {
@@ -67,9 +89,17 @@ class App extends Component {
                   counter: prevState.counter + 1
               }
           });
+      }).catch(() => {
+            this.setState((prevState, props) => {
+                return {
+                    ...props,
+                    loading: false,
+                    errorLoading: true
+                }
+            })
       });
   }
-  toggleShowSpecies = () => {
+  showSpecies = () => {
       this.setState((prevState, props) => {
           return {
               ...props,
@@ -78,22 +108,30 @@ class App extends Component {
       })
   }
   onSpeciesGuessMade = (guess) => {
-      let guessCorrect = guess != null &&
-      guess.ScientificName.toLowerCase() === this.state.scientificName.toLowerCase();
-      if (guessCorrect) {
-          this.getRandomBirdsong();
-      }
+      const guessCorrect = guess != null && guess.ScientificName.toLowerCase() === this.state.scientificName.toLowerCase();
+
       this.setState((prevState, props) => {
-           return {
-               ...props,
-               guessCorrect: guessCorrect,
-               selectedSpeciesGuess: guess
-           }
-        });
+          let correctCount = guessCorrect ? prevState.correctCount + 1 : prevState.correctCount;
+          return {
+              ...props,
+              selectedSpeciesGuess: guess,
+              guessCorrect: guessCorrect,
+              correctCount: correctCount
+          }
+      });
   };
 
+  onChangeExpertMode = (expertMode) => {
+      this.setState((prevState, props) => {
+         return {
+             ...props,
+             expertMode: expertMode
+         }
+      });
+  }
 
-  componentWillMount = function() {
+
+    componentWillMount = function() {
     this.getRandomBirdsong();
   }
   
@@ -105,20 +143,22 @@ class App extends Component {
         </header>
         <div className="App-intro">
             {this.state.loading && <div>Loading...</div>}
-
+            {this.state.errorLoading && <div>Error loading data. Please <a href="#" onClick={() => { this.getRandomBirdsong();}}>try again</a></div>}
                 {!this.state.loading && this.state.noRecordingFound && <span>No recording found</span>}
 
-                {!this.state.loading &&  this.state.birdsongId &&
+                {!this.state.loading &&  !this.state.errorLoading && this.state.birdsongId &&
                 !this.state.noRecordingFound &&
                 <div>
                     <audio autoPlay={true} controls src={`https://www.xeno-canto.org/${this.state.birdsongId}/download`}>
                     </audio>
-                {this.state.showSpecies && <div>{this.state.species}</div>}
                 </div>
                 }
         </div>
+          <input id="expertmode" type="checkbox" onClick={(e) => {
+               this.onChangeExpertMode(e.target.checked);
+          }} style={{marginRight : '3px'}}/><label htmlFor="expertmode">Expert mode</label>
           <div style={{width: '25%', marginLeft: '38%'}}>
-              <Typeahead
+              {!this.state.showSpecies && !this.state.guessCorrect && <Typeahead
                   labelKey="Species"
                   options={this.state.speciesList}
                   placeholder="Type your guess..."
@@ -127,17 +167,29 @@ class App extends Component {
                   onChange={(options) => {
                       this.onSpeciesGuessMade(options[0]);
                   }}
-              />
+                  ref={(ref) => this._typeahead = ref}
+              />}
               {
                   this.state.selectedSpeciesGuess &&
-                  <div>
+                  <div style={{}}>
                       Your guess: <span style={{color: this.state.guessCorrect ? 'green' : 'red'}}>{this.state.selectedSpeciesGuess.Species}</span>
-                      {this.state.guessCorrect ? " Correct" : " Wrong"}
-                   </div>
+                      {this.state.guessCorrect &&
+                      <div>
+                          <div>Correct! </div>
+                          <div>
+                              <a href="#" onClick={() => this.getRandomBirdsong()}>Load next question</a>
+                          </div>
+                      </div>}
+                  </div>
               }
+              {this.state.showSpecies && <div>Answer: {this.state.species} <a href="#" onClick={() => this.getRandomBirdsong()}>Load next question</a></div>}
+
+
           </div>
-          <button onClick={() => this.toggleShowSpecies()}>{this.state.showSpecies ? "Hide species": "Give up?"}</button>
-          <button onClick={() => this.getRandomBirdsong()}>Fetch new song</button>
+          {!this.state.guessCorrect && !this.state.showSpecies && <button onClick={() => this.showSpecies()}>Give up?</button>}
+          <div>
+          Score so far {this.state.correctCount}/{this.state.counter -1 }
+          </div>
       </div>
     );
   }
